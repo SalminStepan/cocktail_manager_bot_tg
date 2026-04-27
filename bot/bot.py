16f252db71ccebfddf77582b3b99da9fe8808193
Stepan Salmin <salmin.stepan.dev@gmail.com>
2026-04-27 13:35:32 +0500

    feat: add basic logging with improve format

--- bot/bot.py
+++ bot/bot.py
@@ -13,7 +13,7 @@ from handlers.edit_handler import edit_router
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
 )
 logger = logging.getLogger(__name__)
 
